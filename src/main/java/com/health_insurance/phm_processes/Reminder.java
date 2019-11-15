19b575e805ee5787e9ad811f97ff0e9298700770
wbadmin <>
2019-11-15 11:39:24 -0500

    {/src/main/java/com/health_insurance/phm_processes/Reminder.java}

--- src/main/java/com/health_insurance/phm_processes/Reminder.java
+++ src/main/java/com/health_insurance/phm_processes/Reminder.java
@@ -61,6 +61,11 @@ public class Reminder implements Serializable {
 	public void setFrom(String from) {
 		this.from = from;
 	}
+	
+	@Override
+	public String toString() {
+	    return "{\"address\":\""+address"\",\"subject\":\""+subject"\",\"body\":\""+body"\",\"from\":\""+from"\"}";
+	}
 
 	public Reminder(String address, String subject,
 			String body, String from) {
