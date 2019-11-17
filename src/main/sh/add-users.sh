da9fc4bb05e577eeba51cb430b3db939794c4c68
mauriziocarioli <maurizio.carioli@gmail.com>
2019-11-17 10:30:26 -0500

    users

--- src/main/sh/add-users.sh
+++ src/main/sh/add-users.sh
@@ -6,10 +6,10 @@ DEMO=~/Demos/rhpam7-install-demo-pgsql
 TARGET=$DEMO/target
 JBOSS_EAP=jboss-eap-7.2
 JBOSS_HOME=$TARGET/$JBOSS_EAP
-$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u rxs -p redhatpam1! -ro user,CCN --silent
-$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u mch -p redhatpam1! -ro user,CCN --silent
-$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u pea -p redhatpam1! -ro user,CCN --silent
-$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u chw -p redhatpam1! -ro user,CCN --silent
-$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u mrx -p redhatpam1! -ro user,CCN --silent
-$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u mem -p redhatpam1! -ro user,MLP --silent
-$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u pro -p redhatpam1! -ro user,MCC --silent
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u Robert -p redhatpam1! -ro user,RXS --silent
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u Marc -p redhatpam1! -ro user,MCH --silent
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u Patricia -p redhatpam1! -ro user,PEA --silent
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u Charlie -p redhatpam1! -ro user,CHW --silent
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u Matthew -p redhatpam1! -ro user,MRX --silent
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u Mary -p redhatpam1! -ro user,MEM --silent
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u Peter -p redhatpam1! -ro user,PRO --silent
